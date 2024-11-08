33fc9f7c45e721bf91bdc8505f2dd9b1197297d2
GOH01 <wave4446@naver.com>
2024-11-08 17:49:47 +0900

    first commit

---
+++ src/main/java/Javaproject/Feellog/FeellogApplication.java
@@ -0,0 +1,13 @@
+package Javaproject.Feellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeellogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeellogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Javaproject/Feellog/FeellogApplicationTests.java
@@ -0,0 +1,13 @@
+package Javaproject.Feellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeellogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
